c73a04ec015c1e7f55f8da0afe0a9916a1f786c5
Justin <mancussion@gmail.com>
2025-02-03 16:56:49 -0500

    connected to ai

--- SC3000/ai_sportcaster/urls.py
+++ SC3000/ai_sportcaster/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import summarize_game, test_page
+from .views import summarize_game, test_page, SummarizeGameView
 
 urlpatterns = [
     path('summarize/', summarize_game, name='summarize_game'),
     path('test/', test_page, name='test_page'),
+    path('test2/', SummarizeGameView.as_view(), name="test2")
 ]
--- SC3000/ai_sportcaster/views.py
+++ SC3000/ai_sportcaster/views.py
@@ -2,6 +2,12 @@ from django.shortcuts import render
 from django.http import JsonResponse
 from django.views.decorators.http import require_POST
 from .summarize_layer import get_gemini_summary
+from accounts.models import Favorite_Teams, Teams
+from django.views.generic import TemplateView
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.urls import reverse_lazy
+from django.contrib import messages
+import statsapi
 
 @require_POST
 def summarize_game(request):
@@ -23,3 +29,50 @@ def summarize_game(request):
 
 def test_page(request):
     return render(request, 'ai_sportcaster/test_ai.html')
+
+class SummarizeGameView(LoginRequiredMixin, TemplateView):
+    template_name = "ai_sportcaster/test_ai.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        user = self.request.user
+        fav_teams = Favorite_Teams.objects.filter(user_id=user.id)
+        context["favorite_teams"] = fav_teams
+        game_data = []
+        for team in fav_teams:
+            team_obj = Teams.objects.get(id=team.id)
+            game_id = statsapi.last_game(team_obj.mlb_id)
+            highlight_data = statsapi.game_scoring_play_data(game_id)
+            game_data.append(highlight_data)
+        games = []
+        for game in game_data:
+            game_obj = {
+                "home": game["home"],
+                "away": game["away"],
+                "plays": [],
+                "score": {
+                    "home": "",
+                    "away": "",
+                }
+            }
+            highlights= []
+            plays = game["plays"]
+            for events in plays:
+                result = events["result"]
+                highlights.append(result)
+            if highlights:
+                last_score = highlights[-1]
+                score = game_obj["score"]
+                score["home"] = last_score["homeScore"]
+                score["away"] = last_score["awayScore"]
+            else:
+                score["home"] = 0
+                score["away"] = 0
+                highlights.append({"description":"Game cancelled.", "homeScore": 0, "awayScore": 0})
+            game_obj["plays"] = highlights
+            games.append(game_obj)
+        context["games"] = games
+        summary = get_gemini_summary(f"{games}")
+        context["summary"] = summary
+
+        return context
