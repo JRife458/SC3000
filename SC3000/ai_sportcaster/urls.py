94567be9a281d76a1e952bedfee834c82ff078ca
Justin <mancussion@gmail.com>
2025-02-04 14:10:32 -0500

    final bug fixing

--- SC3000/ai_sportcaster/urls.py
+++ SC3000/ai_sportcaster/urls.py
@@ -6,7 +6,7 @@ from .views import tts_test_page
 app_name = "ai_sportcaster"
 
 urlpatterns = [
-    path('test/', SummarizeGameView.as_view(), name="test"),
-    path('speak/', speak_text, name='speak_text'),
-    path('tts-test/', tts_test_page, name='tts_test_page'),
+    # path('test/', SummarizeGameView.as_view(), name="test"),
+    # path('speak/', speak_text, name='speak_text'),
+    # path('tts-test/', tts_test_page, name='tts_test_page'),
 ]
